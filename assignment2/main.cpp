bb4a332f20226e35c3b540eaa296efa90cc9fda5
LINYV <3092935750@qq.com>
2025-12-09 11:31:26 +0800

    finish assignment 2

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -13,8 +13,9 @@
 #include <set>
 #include <string>
 #include <unordered_set>
+#include <sstream>
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "Aziel Le"; // Don't forget to change this!
 
 /**
  * Takes in a file name and returns a set containing all of the applicant names as a set.
@@ -28,8 +29,36 @@ std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
  * to also change the corresponding functions in `utils.h`.
  */
 std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+  std::ifstream ifs(filename);
+  if(!ifs.is_open()){
+    exit;
+  }
+  std::set<std::string> students;
+  std::string newline;
+  while(std::getline(ifs,newline)){
+     students.insert(newline);
+  }
+  return students;
 }
+
+bool find_initials_matches(std::string kYourName,std::string name){
+    std::stringstream myname(kYourName);
+    std::string my_first_name;
+    std::string my_second_name;
+    std::stringstream othername(name);
+    std::string first_name;
+    std::string second_name;
+    myname>>my_first_name>>my_second_name;
+    othername>>first_name>>second_name;
+    bool first_judge = (my_first_name.at(0)==first_name.at(0));
+    bool second_judge = (my_second_name.at(0)==second_name.at(0));
+    if(first_judge&&second_judge){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
 
 /**
  * Takes in a set of student names by reference and returns a queue of names
@@ -40,7 +69,14 @@ std::set<std::string> get_applicants(std::string filename) {
  * @return          A queue containing pointers to each matching name.
  */
 std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
+  std::queue<const std::string*> match_names;
+  for(const auto& student:students){
+     if(find_initials_matches(name,student)){
+       const std::string* student_refference = &student;
+       match_names.push(student_refference);
+     }
+  }
+  return match_names;
 }
 
 /**
@@ -54,7 +90,20 @@ std::queue<const std::string*> find_matches(std::string name, std::set<std::stri
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
 std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
+  std::string match_name;
+  if(matches.empty()){
+    printf("NO MATCHES FOUND.");
+    exit;
+  }
+  else{
+    int extract_number = matches.size()-2;
+    for (int i = 0; i < extract_number; i++)
+    {
+      matches.pop();
+    }
+    match_name = *matches.front();
+  }
+  return match_name;
 }
 
 /* #### Please don't remove this line! #### */
