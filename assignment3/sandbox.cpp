dbd8185be8a79dbd588f2bbf870c1d0d48b0f2ba
LINYV <3092935750@qq.com>
2025-12-11 18:41:00 +0800

    finish the assignment3

--- assignment3/sandbox.cpp
+++ assignment3/sandbox.cpp
@@ -2,7 +2,38 @@
  * CS106L Assignment 3: Make a Class
  * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
  */
+#include "class.h"
+#include <string>
+#include <iostream>
 
 void sandbox() {
-  // STUDENT TODO: Construct an instance of your class!
+    myclass<std::string> math_class("math","Lee",5);
+    try {
+    math_class.addStudent("Flanc");
+    math_class.addStudent("Jane");
+    math_class.addStudent("Owe");
+    math_class.addStudent("Lowis");
+    math_class.addStudent("James");
+    } catch (const std::exception& e) {
+    std::cerr << "Oops! Something went wrong: " << e.what() << '\n';
+    }
+    math_class.getStudentAt(3);
+    try{
+        math_class.printRollCall();
+    }catch(const std::exception& e){
+        std::cerr << "Oops! Something went wrong: " << e.what() << '\n';
+    }
+    try{
+        math_class.addStudent("Lee");
+        math_class.printRollCall();
+    }catch(const std::exception& e){
+        std::cerr << "Oops! Something went wrong: " << e.what() << '\n';
+    }
+    try{
+        math_class.removeLastStudent();
+        math_class.printRollCall();
+    }catch(const std::exception& e){
+        std::cerr << "Oops! Something went wrong: " << e.what() << '\n';
+    }
+
 }
